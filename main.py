27ab9fdc34249c888be57bf3884eef21a69fa9e0
Teo Heres <heresteodor@gmail.com>
2025-10-31 21:44:59 +0200

    First tag and attribute search

--- main.py
+++ main.py
@@ -3,4 +3,20 @@ import requests
 
 url = 'http://quotes.toscrape.com'
 
-print(requests.get(url))
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, 'lxml')
+# print(soup)
+
+#print(soup.find_all('p', class_ = "copyright"))
+
+#print(soup.find('span', class_ = "text").text.strip())
+
+print(soup.find_all('span', itemprop = 'text')) #finds all items with 'span' tag and itemprop atribute
+
+first_p = soup.find('span', itemprop = 'text') #finds the first item with those attributes
+
+second_p = first_p.find_next('span', itemprop = 'text') # finds the next item with those attributes
+
+print(first_p.text)
+print(second_p.text)
